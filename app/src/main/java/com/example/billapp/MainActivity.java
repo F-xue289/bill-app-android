ac9dcb0488d74b9d3d3fe61a624fa5ff4ec63626
F-xue289 <3549036223@qq.com>
2026-05-08 15:29:50 +0800

    feat: week8 complete add/query/delete function 完成增查删功能

--- app/src/main/java/com/example/billapp/MainActivity.java
+++ app/src/main/java/com/example/billapp/MainActivity.java
@@ -112,7 +112,7 @@ public class MainActivity extends AppCompatActivity implements BillAdapter.OnBil
             mTotalAmountTextView.setTextColor(0xFF4CAF50);
             mTotalAmountTextView.setText(String.format("¥ +%.2f", netAmount));
         } else {
-            // 支出大于收入，显示红色，使用-号
+            // 支出大于收入，显示红色，使用-
             mTotalAmountTextView.setTextColor(0xFFF44336);
             mTotalAmountTextView.setText(String.format("¥ %.2f", netAmount));
         }
